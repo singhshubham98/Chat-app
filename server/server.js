4b7072fbd3f61cb7c5ea50933484a21bf49be490
Shubham Singh <shubhamsingh4204@gmail.com>
2018-09-16 11:20:37 +0530

    Add newMessage and createMessage

--- server/server.js
+++ server/server.js
@@ -13,9 +13,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket)=>{
     console.log('New user connected');
 
+// socket.emit('newEmail',{
+//     from: 'example@gmail.com',
+//     text: 'New mail',
+//     createdAt: 123
+// });
+
+socket.emit('newMessage', {
+    from: 'shubham',
+    text: 'Hey shubham',
+    createdAt: 123
+});
+
+socket.on('createMessage', (mess)=>{
+    console.log('Create message:', mess);
+});
+
+// socket.on('createEmail', (newEmail)=>{
+//     console.log('New Email:', newEmail);
+// });
+
 socket.on('disconnect', ()=>{
     console.log('User was disconected');
 });
+
 });
 server.listen(port,()=>{
 
