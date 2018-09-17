f05b6eed0b338644d1c3276ae794d4e58270e302
Shubham Singh <shubhamsingh4204@gmail.com>
2018-09-17 10:01:54 +0530

    File Fixes

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+var socket = io();
+
+socket.on('connect', function(){
+    console.log('connected to server');
+
+});
+
+socket.on('disconnect', function(){
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message){
+    console.log('New Message:', message);
+});
